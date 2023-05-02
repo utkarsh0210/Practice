d6affaa8cf97475d5b221779eb5ed69d3034f16b
utkarsh0210 <116905545+utkarsh0210@users.noreply.github.com>
2023-05-03 01:26:45 +0530

    Add files via upload

---
+++ tur.py
@@ -0,0 +1,11 @@
+import turtle
+t=turtle.Turtle()
+s=turtle.Screen()
+s.bgcolor('black')
+t.pencolor('blue')
+t.speed(0)
+for i in range(200):
+    t.circle(190-1,90)
+    t.lt(98)
+    t.circle(190-1,90)
+    t.lt(18)
